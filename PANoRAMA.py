1247c2ad91722e2cb03e20b6003b75576d217d29
sgrieve <s.grieve@ed.ac.uk>
2016-11-11 15:47:02 +0000

    inital commit

---
+++ PANoRAMA.py
@@ -0,0 +1,25 @@
+import Image
+from images2gif import writeGif
+
+
+a = Image.open('/home/sgrieve/Pictures/pano.png')
+
+step = 10
+
+w, h = a.size
+final_width = int(h * 1.5)
+
+images = []
+
+for i in range(0, w - final_width, step):
+    images.append(a.crop((i, 0, final_width + i, h)))
+
+for i in range(w - final_width, 0, -step):
+    images.append(a.crop((i, 0, final_width + i, h)))
+
+
+for b, j in enumerate(images):
+    j.save('/home/sgrieve/Pictures/tmp/' + str(b).zfill(4) + '.png')
+
+#writeGif('test.gif', images, duration=.15, dither=0.)
+#images[-1].save('tmp.jpg')
