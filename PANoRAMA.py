ac5f29e27fd6c2d17aa2a5b72c3031688355158d
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-09-03 19:53:21 +0100

    using new pillow version to create gifs

--- PANoRAMA.py
+++ PANoRAMA.py
@@ -1,22 +1,23 @@
-import Image
-from images2gif import writeGif
+from PIL import Image
 
+a = Image.open('input.jpg')
 
-a = Image.open('/home/sgrieve/Pictures/pano.png')
-
-step = 10
+step = 100
 
 w, h = a.size
 final_width = int(h * 1.5)
-
 images = []
 
 for i in range(0, w - final_width, step):
     images.append(a.crop((i, 0, final_width + i, h)))
 
+# This leaves a black screen at the end of each loop
+# images = images + images[::-1]
+
+# This second loop should be replaced by the slicing above
 for i in range(w - final_width, 0, -step):
     images.append(a.crop((i, 0, final_width + i, h)))
 
-
-for b, j in enumerate(images):
-    j.save('/home/sgrieve/Pictures/tmp/' + str(b).zfill(4) + '.png')
+output = Image.new(a.mode, (final_width, h))
+output.save('hello.gif', save_all=True, append_images=images, duration=200,
+            loop=0)
