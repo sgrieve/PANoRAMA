ba1e61c8d3814e682375d99b1f71fa8e1390fbab
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-09-05 09:48:51 +0100

    Adding test skeleton

---
+++ tests/test_panorama.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+class TestPanorama():
+    def test_file_load_correct_file(self):
+        pass
+
+    def test_file_load_missing_file(self):
+        pass
+
+    def test_file_load_non_image_file(self):
+        pass
+
+    def test_save_file_correct(self):
+        pass
+
+    def test_save_file_file_exists(self):
+        pass
+
+    def test_save_file_path_does_not_exist(self):
+        pass
+
+    def test_save_invalid_format(self):
+        pass
+
+    def test_full_run_valid_input(self):
+        pass
+
+    def test_full_run_invalid_input(self):
+        pass
+
+    def test_full_run_non_panorama_image(self):
+        pass
